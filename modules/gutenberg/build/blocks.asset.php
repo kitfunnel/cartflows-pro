b28ac72378d01a744a4a4a077c54431c1a496ab6
KitFunnel <kitfunnel@gmail.com>
2022-11-10 12:38:01 -0500

    CartFlows Pro 1.11.3

---
+++ modules/gutenberg/build/blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '7e31b21bcec7e3f08bedda59624bcce0');
