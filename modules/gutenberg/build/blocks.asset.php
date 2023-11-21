c27258259aaf0e62cde42fc93946e1497bab9423
kitfunnel <111517579+kitfunnel@users.noreply.github.com>
2023-11-20 22:04:11 -0500

    Clear

--- modules/gutenberg/build/blocks.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '3d74689e68ed018c9bd8824c6073e685');
